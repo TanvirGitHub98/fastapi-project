926daae57eff8292146830edee8cdfb6a4e694b8
Tanvir Hasan <reyadtanvir2052@gmail.com>
2025-03-28 12:06:33 +0900

    second pus

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 app=FastAPI()
 
-class User(BaseModel):
+class Post(BaseModel):
     title:str
-    content:int =Field(strict=True)
+    content:str
     published:bool=True
  
 try:
@@ -29,7 +29,7 @@ def checkId(id):
          
 
 
-def findUserForById(id):
+def findPostForById(id):
     for i,p in enumerate(myUser):
         if p['id']==id:
             return i
@@ -38,48 +38,52 @@ def findUserForById(id):
 
 @app.get('/')
 async def root():
-    return{"All User":myUser}
+    cursor.execute("""SELECT * FROM posts """)
+    posts=cursor.fetchall()
+    return{"All Post":posts}
 
-@app.post('/createAccount',status_code=status.HTTP_201_CREATED)
-async def createAccount(newUser:User):
-    userDict=newUser.model_dump()
-    userDict['id']=randrange(0,1000000)
-    myUser.append(userDict)
+@app.post('/createPost',status_code=status.HTTP_201_CREATED)
+async def createPost(post:Post):
+   cursor.execute("""INSERT INTO posts(title,content,published) VALUES(%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
+   new_post=cursor.fetchone()
+   conn.commit()
+   return{
+       "new_post":new_post
+   }
+  
     
-    
-    return{
-        "new User": myUser
-    }
-    
-@app.get('/getById/{id}')
-async def getById(id:int):
-    Id=checkId(id)
-    if not Id:
+@app.get('/postgetById/{id}')
+async def postGetById(id:int):
+    cursor.execute("""SELECT * FROM posts WHERE ID=%s """,(str(id)))
+    post=cursor.fetchone()
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id :{id} not found")
         return
     return{
-        "Id":Id
+        "Id":post
     }
     
     
-@app.delete('/deleteUserById/{id}',status_code=status.HTTP_204_NO_CONTENT)
-async def deleteUserById(id:int):
+@app.delete('/deletePostById/{id}',status_code=status.HTTP_204_NO_CONTENT)
+async def deletePostById(id:int):
     
-    res=findUserForById(id)
-    if res is None:
+    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id)))
+    deleted_post=cursor.fetchone()
+    conn.commit()
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User Id: {id} Not Found")
     else:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
         
 
-@app.put('/updateUserById/{id}')
-async def updateUser(id:int,user:User):
-    index=findUserForById(id)
-    if index is None:
+@app.put('/updatePostById/{id}')
+async def updatePost(id:int,post:Post):
+    cursor.execute("""UPDATE posts SET  title=%s, content=%s,published=%s  WHERE ID=%s RETURNING *""",(post.title,post.content,post.published,str(id)))
+    updated_Post=cursor.fetchone()
+    conn.commit()
+    
+    if updated_Post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Id {id} is not available")
 
-    userData=user.model_dump()
-    userData['id']=id
-    myUser[index]=userData
-    return {"message": "User updated successfully", "updatedUser": userData}
+    return {"message": "Post updated successfully", "updatedPost": updated_Post}
 
